4b8a168911284cc114f411bc34d3f1b14e5ba5f4
Christian <csaldanabanuelos@gmail.com>
2025-09-12 00:36:22 -0500

    [dyad] wrote 1 file(s)

--- src/components/image-viewer-dialog.tsx
+++ src/components/image-viewer-dialog.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogClose } from "@/components/ui/dialog";
 import Image from "next/image";
+import { X } from "lucide-react"; // Import the X icon
 
 interface ImageViewerDialogProps {
   src: string | null;
@@ -19,7 +20,7 @@ export function ImageViewerDialog({
 }: ImageViewerDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="flex w-auto max-w-[90vw] max-h-[90vh] items-center justify-center p-0">
+      <DialogContent className="flex w-auto max-w-[90vw] max-h-[90vh] items-center justify-center p-0 border-none bg-transparent">
         {src && (
           <img
             src={src}
@@ -27,6 +28,9 @@ export function ImageViewerDialog({
             className="max-h-full max-w-full object-contain"
           />
         )}
+        <DialogClose className="absolute top-4 right-4 rounded-full bg-black/50 p-2 text-white hover:bg-black/70 transition-colors">
+          <X className="h-6 w-6" /> {/* Larger X icon */}
+        </DialogClose>
       </DialogContent>
     </Dialog>
   );
