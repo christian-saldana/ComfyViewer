3e731398f87022e1aba465064aa5ec5406e870b6
Christian <csaldanabanuelos@gmail.com>
2025-09-13 18:21:43 -0500

    Reverted all changes back to version ea78bae4047ae08e3b900d1039e41558bfb492af

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,54 +1,38 @@
 "use client"
 
-import * as React from "react"
-import {
-  Panel as PanelPrimitive,
-  PanelGroup as PanelGroupPrimitive,
-  PanelResizeHandle as PanelResizeHandlePrimitive,
-} from "react-resizable-panels"
+import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = React.forwardRef<
-  React.ElementRef<typeof PanelGroupPrimitive>,
-  React.ComponentPropsWithoutRef<typeof PanelGroupPrimitive>
->(({ className, ...props }, ref) => (
-  <PanelGroupPrimitive
-    ref={ref}
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+  <ResizablePrimitive.PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-))
-ResizablePanelGroup.displayName = "ResizablePanelGroup"
+)
 
-const ResizablePanel = PanelPrimitive
+const ResizablePanel = ResizablePrimitive.Panel
 
-const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof PanelResizeHandlePrimitive>,
-  React.ComponentPropsWithoutRef<typeof PanelResizeHandlePrimitive> & {
-    withHandle?: boolean
-  }
->(({ className, withHandle, children, ...props }, ref) => (
-  <PanelResizeHandlePrimitive
-    ref={ref}
+const ResizableHandle = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}) => (
+  <ResizablePrimitive.PanelResizeHandle
     className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      "relative flex w-2 items-center justify-center bg-transparent after:absolute after:inset-y-0 after:left-1/2 after:w-[5px] after:-translate-x-1/2 after:bg-border after:transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-id]]:focus-visible:z-10 data-[resize-handle-state=hover]:after:bg-primary data-[resize-handle-state=drag]:after:bg-primary",
       className
     )}
     {...props}
   >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <div className="h-2.5 w-px bg-muted-foreground" />
-      </div>
-    )}
-    {children}
-  </PanelResizeHandlePrimitive>
-))
-ResizableHandle.displayName = "ResizableHandle"
+  </ResizablePrimitive.PanelResizeHandle>
+)
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
 export type { ImperativePanelHandle } from "react-resizable-panels"
