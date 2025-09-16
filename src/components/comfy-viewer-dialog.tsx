afad8e8368f7a7624896e7ff31fe34d323d292fa
Christian <csaldanabanuelos@gmail.com>
2025-09-15 19:39:26 -0500

    fix: annoying console warning

--- src/components/comfy-viewer-dialog.tsx
+++ src/components/comfy-viewer-dialog.tsx
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 
+import { DialogDescription } from "@radix-ui/react-dialog";
+
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 
 interface ComfyViewerDialogProps {
@@ -21,6 +23,7 @@ export function ComfyViewerDialog({
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="flex w-auto max-w-[90vw] max-h-[90vh] items-center justify-center p-0 border-none bg-transparent">
         <DialogTitle className="sr-only">{alt}</DialogTitle>
+        <DialogDescription className="sr-only">Full-screen view of the image: {alt}</DialogDescription>
         {src && (
           <img
             src={src}
